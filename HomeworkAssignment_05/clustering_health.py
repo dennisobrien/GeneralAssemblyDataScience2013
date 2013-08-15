4d135baa7d653ed6ea2be399e8f698c86b0b5944
Dennis O'Brien <dennis@dennisobrien.net>
2013-08-15 10:23:10 -0700

    Assignment 5 - loading the data, nothing else.  We have a dataset.

---
+++ HomeworkAssignment_05/clustering_health.py
@@ -0,0 +1,36 @@
+"""Perform k-means clustering on a dataset containing various health metrics
+for countries as gathered by The World Bank.
+
+http://data.worldbank.org/topic/health
+http://api.worldbank.org/datafiles/8_Topic_MetaData_en_EXCEL.xls
+
+Requirements:
+pandas  Version 0.12+
+xlrd    For Excel file reading.  (pip install xlrd)
+"""
+
+import os
+import pandas as pd
+import requests
+
+
+def get_datafile():
+    datafile_url = 'http://api.worldbank.org/datafiles/8_Topic_MetaData_en_EXCEL.xls'
+    filepath = os.path.basename(datafile_url)
+    if not os.path.exists(filepath):
+        print("downloading {}".format(datafile_url))
+        r = requests.get(datafile_url, stream=True)
+        if r.status_code == 200:
+            with open(filepath, 'wb') as f:
+                for chunk in r.iter_content(1024):
+                    f.write(chunk)
+            print("downloaded to {}".format(filepath))
+    return filepath
+
+def main():
+    data_filepath = get_datafile()
+    df = pd.read_excel(data_filepath, 'Sheet1')
+    print df
+
+if __name__=='__main__':
+    main()
