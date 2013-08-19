0f5247fe351c5363bae3f442af077865d4f7a2ff
Dennis O'Brien <dennis@dennisobrien.net>
2013-08-18 21:34:13 -0700

    Plot the distributions of each of the metrics to get an idea of how they are shaped. Plot some pairs of metrics.

--- HomeworkAssignment_05/clustering_health.py
+++ HomeworkAssignment_05/clustering_health.py
@@ -15,6 +15,33 @@ import pandas as pd
 import requests
 
 
+def plot_histograms_for_each_column(df,n_columns=3, indicator_map=None, 
+                                    n_points_threshold=20, verbose=False):
+    n_subplots = sum([df.count()[column_name] > n_points_threshold for column_name in df.columns])
+    n_rows = (n_subplots/n_columns) + 1
+    plot_width = 6
+    plot_height = 4
+    figsize = (plot_width*n_columns, plot_height*n_rows)
+    fig, axes = plt.subplots(nrows=n_rows, ncols=n_columns, figsize=figsize)
+    idx = 0
+    for column_name in df.columns:
+        n_points = df.count()[column_name]
+        if n_points >= n_points_threshold:
+            i_row = idx / n_columns
+            i_col = idx % n_columns
+            idx += 1
+            if indicator_map is not None and column_name in indicator_map:
+                column_description = indicator_map[column_name]
+            else:
+                column_description = column_name
+            title = column_description
+            df[column_name].hist(ax=axes[i_row,i_col])
+            axes[i_row,i_col].set_title(title)
+        else:
+            if verbose:
+                print("Skipping {}, too few points: {}".format(column_name, n_points))
+    return fig
+
 def get_datafile():
     datafile_url = 'http://api.worldbank.org/datafiles/8_Topic_MetaData_en_EXCEL.xls'
     filepath = os.path.basename(datafile_url)
